b03cfe2d6e363b6737acdb2f83ae3582b7cf8e24
Satyam Singh <ssatsin2308@gmail.com>
2023-12-11 01:02:39 +0530

    Contains Duplicate

---
+++ Contains Duplicates 217.java
@@ -0,0 +1,32 @@
+class Solution {
+    // Approach - 1 Using HashSet
+    public boolean containsDuplicate(int[] nums) {
+        HashSet<Integer> set = new HashSet<>();
+
+        for(int i=0;i<nums.length;i++) {
+            if(set.contains(nums[i])) {
+                return true;
+            } else {
+                set.add(nums[i]);
+            }
+        }
+        return false;
+    }
+}
+
+
+class Solution {
+    // Approach - 2 Using Sorting -> O(n) + O(nlogn) = ~ O(n)
+    public boolean containsDuplicate(int[] nums) {
+        Arrays.sort(nums); // o(nlog n)
+
+        int prev = nums[0];
+        for(int i=1; i<nums.length; i++) {  // O(n)
+            if(prev == nums[i]) {
+                return true;
+            }
+            prev = nums[i];
+        }
+        return false;
+    }
+}
--- README.md
+++ README.md
@@ -3,3 +3,4 @@ Blind 75 -> https://leetcode.com/discuss/interview-question/460599/Blind-75-Leet
 Questions Solved
 1. Two Sum -> https://leetcod.com/problems/two-sum/description/
 2. Best Time to Buy and Sell Stock  -> https://leetcode.com/problems/best-time-to-buy-and-sell-stock/description/
+3. Contains Duplicate -> https://leetcode.com/problems/contains-duplicate/description/ 
